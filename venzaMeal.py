6c74ebba6ad4656f6a86a84ce17a8cdf9d6596b4
Dominik Bencko <dominik.bencko@gmail.com>
2016-11-08 13:47:52 +0100

    My first web scraper

---
+++ venzaMeal.py
@@ -0,0 +1,15 @@
+import requests
+from lxml import html
+
+page = requests.get('http://studentskejedalne.sk')
+tree = html.fromstring(page.content)
+meals = tree.xpath('//td/text()')
+
+venza = False
+for i in meals:
+    if "Špagety" in i:
+        venza = True
+        break
+
+if venza:
+    print("YES")
